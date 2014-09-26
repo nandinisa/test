096a028c949cda3e9c061456e35d99539a7eb602
nandinisa <nandini.05@hotmail.com>
2014-09-27 00:51:27 +1000

    Removed proj references

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var debug = require('debug')('angularjs-sample');
 var app = require('./init');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 1337);
 
 var server = app.listen(app.get('port'), function() {
   debug('Express server listening on port ' + server.address().port);
