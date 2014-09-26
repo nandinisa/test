e132900bc70158d52bca032a7834541cd08f8935
nandinisa <nandini.05@hotmail.com>
2014-09-27 00:24:45 +1000

    Node js cloud project

--- init.js
+++ init.js
@@ -5,21 +5,21 @@ var path = require('path');
 
 var connect = require('connect');
 
-var server = express();
+var init = express();
 
-server.use(connect.json());
-server.use(connect.urlencoded());
+init.use(connect.json());
+init.use(connect.urlencoded());
 
 // Set routing handler to serve all static files
 // Set the directory public to serve static files
-server.use(express.static(path.join(__dirname, 'client')));
+init.use(express.static(path.join(__dirname, 'client')));
 
 
 // Set error handlers
 // Assign a function as a handler, where error is passed as the first object
 // Check for test environment and if yes, then pass on the details of the error
-if (server.get('NODE_ENV') === 'test') {
-    server.use(function (err, req, res, next) {
+if (init.get('NODE_ENV') === 'test') {
+    init.use(function (err, req, res, next) {
         res.status(err.status || 500).json({
             'title': err.message,
             'details': err
@@ -29,7 +29,7 @@ if (server.get('NODE_ENV') === 'test') {
 
 // Production error handler
 // No stacktraces leaked to user
-server.use(function (err, req, res, next) {
+init.use(function (err, req, res, next) {
     res.status(err.status || 500).json({
         'title': err.message,
         'details': {}
