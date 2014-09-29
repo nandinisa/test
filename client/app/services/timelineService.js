b0bc7e9f998d63056352a2d74eaf897c89246c96
nandinisa <nandini.05@hotmail.com>
2014-09-29 10:18:47 +1000

    Added timeline service

---
+++ client/app/services/timelineService.js
@@ -0,0 +1,18 @@
+﻿'use strict';
+app.factory('timelineService', ['$http', '$cookies', 'ngAuthSettings', function ($http, $cookies, ngAuthSettings) {
+
+    var serviceBase = ngAuthSettings.apiServiceBaseUri;
+
+    var timelineServiceFactory = {};
+
+    var _getTimeline = function () {
+        return $http.get(serviceBase + 'api/timeline',{withCredentials: true}).then(function (results) {
+            return results;
+        });
+    };
+
+    timelineServiceFactory.getTimeline = _getTimeline;
+
+    return timelineServiceFactory;
+
+}]);
