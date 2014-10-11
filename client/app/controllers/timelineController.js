15add6e752ea19d726d13795b7973b8a7d566075
nandinisa <nandini.05@hotmail.com>
2014-10-11 15:14:49 +1100

    changes

--- client/app/controllers/timelineController.js
+++ client/app/controllers/timelineController.js
@@ -2,13 +2,30 @@
 app.controller('timelineController', ['$scope', 'timelineService', function ($scope, timelineService) {
 
     $scope.timelines = [];
+    //var serviceBase = 'http://localhost:43868/';
+    var serviceBase = 'http://6b74cc2ca3764a3db82c8499ef1795aa.cloudapp.net/';
+    var xhr = new XMLHttpRequest()
+    xhr.open("GET", serviceBase + 'api/timeline', true);
+    xhr.withCredentials = true
+    xhr.onreadystatechange = function () {
+        if (this.status === 200) {
+            if (this.responseText !== '') {
+                $scope.timelines = JSON.parse(this.responseText);
+            }
+            else {
+                $scope.timelines = '';
+            }
+            $scope.$apply();
+        }
+    }
+    xhr.send();
 
-    timelineService.getTimeline().then(function (results) {
+    //timelineService.getTimeline().then(function (results) {
 
-        $scope.timelines = results.data;
+    //    $scope.timelines = results.data;
 
-    }, function (error) {
-        //alert(error.data.message);
-    });
+    //}, function (error) {
+    //    //alert(error.data.message);
+    //});
 
 }]);
--- client/app/services/timelineService.js
+++ client/app/services/timelineService.js
@@ -9,6 +9,18 @@ app.factory('timelineService', ['$http', 'ngAuthSettings', function ($http, ngAu
         return $http.get(serviceBase + 'api/timeline', {withCredentials: true}).then(function (results) {
             return results;
         });
+        
+        //var xhr = new XMLHttpRequest()
+        //xhr.open("GET", serviceBase + 'api/timeline', true);
+        //xhr.withCredentials = true
+        //xhr.onreadystatechange = function () {
+        //    if (this.status === 200) {
+        //        results = JSON.parse(this.responseText);
+        //        onload(results);
+        //    }
+        //}
+        //xhr.send();
+
     };
 
     timelineServiceFactory.getTimeline = _getTimeline;
